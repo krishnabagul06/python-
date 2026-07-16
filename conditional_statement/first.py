5c94e2d0edf39df31fe70bc86361862a155c68d2
krishnabagul06 <krishnabagul06@gmail.com>
2026-07-16 13:24:53 +0530

    conditional statement

---
+++ conditional_statement/first.py
@@ -0,0 +1,16 @@
+# WAP to enter a number between 1 to 5 and print its word formate (use only if statement)
+
+n=int(input("Enter a number between 1 to 5: "))
+if n==1:
+    print("One")
+if n==2:
+    print("Two")
+if n==3:
+    print("Three")
+if n==4:
+    print("Four")
+if n==5:
+    print("Five")
+if n<1 or n>5:
+    print("Invalid input")
+    
